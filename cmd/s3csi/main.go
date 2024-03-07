a3791b4ef1e494e0e391fb49add8fc4e098ce40c
liuxu <liuxu@wxchina.com>
2024-03-07 17:08:05 +0800

    1.实现启动Driver代码部分 2.增加Dockerfile以及构建的脚本

--- cmd/s3csi/main.go
+++ cmd/s3csi/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"flag"
+
+	"github.com/liuxuzxx/csi-s3/pkg/driver"
+	"k8s.io/klog"
+)
+
+var (
+	endpoint string
+	nodeId   string
+)
+
 func main() {
-	println("Start S3 CSI Driver...")
+	println("Start S3 CSI...")
+	flag.StringVar(&endpoint, "endpoint", "", "CSI Endpoint")
+	flag.StringVar(&nodeId, "nodeId", "", "node id")
+	klog.InitFlags(nil)
+	flag.Parse()
+	d := driver.NewDriver(nodeId, endpoint)
+	d.Run()
+	println("End S3 CSI...")
 }
