395142d7bed1e34d44043e6ef36fd75d85fa2f53
liuxu <liuxu@wxchina.com>
2024-03-14 19:14:03 +0800

    1.完善使用mount-s3命令执行挂载这个操作

--- pkg/driver/nodeserver.go
+++ pkg/driver/nodeserver.go
@@ -1,11 +1,15 @@
 package driver
 
 import (
+	"os"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/liuxuzxx/csi-s3/pkg/s3"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"k8s.io/klog"
+	"k8s.io/mount-utils"
 )
 
 type NodeServer struct {
@@ -20,8 +24,26 @@ func NewNodeServer(nodeId string) *NodeServer {
 
 // 格式化磁盘 Mount到全局目录
 func (n *NodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
-
 	klog.V(4).Infof("NodeStageVolume: called with args %+v", *req)
+
+	volumeId := req.GetVolumeId()
+	path := req.GetStagingTargetPath()
+	if len(volumeId) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume Id missing in request")
+	}
+
+	if len(path) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path missing in request")
+	}
+	notMnt, err := checkMount(path)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	//如果是已经挂载了，那么直接返回响应即可
+	if !notMnt {
+		return &csi.NodeStageVolumeResponse{}, nil
+	}
+
 	return &csi.NodeStageVolumeResponse{}, nil
 }
 
@@ -43,6 +65,19 @@ func (ns *NodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 		return nil, status.Error(codes.InvalidArgument, "Target path not provided")
 	}
 
+	notMnt, err := checkMount(target)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if !notMnt {
+		return &csi.NodePublishVolumeResponse{}, nil
+	}
+	mnt := s3.NewMountpointS3("k8s-dev-sc")
+	err = mnt.Mount(volumeId, target)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
 	return &csi.NodePublishVolumeResponse{}, nil
 }
 
@@ -84,4 +119,19 @@ func (ns *NodeServer) NodeGetVolumeStats(ctx context.Context, in *csi.NodeGetVol
 
 func (ns *NodeServer) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandVolumeRequest) (*csi.NodeExpandVolumeResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func checkMount(path string) (bool, error) {
+	notMnt, err := mount.New("").IsLikelyNotMountPoint(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err = os.MkdirAll(path, 0750); err != nil {
+				return false, err
+			}
+			notMnt = true
+		} else {
+			return false, err
+		}
+	}
+	return notMnt, nil
 }
---
+++ pkg/s3/mountpoint.go
@@ -0,0 +1,54 @@
+package s3
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"k8s.io/klog"
+)
+
+const (
+	mountS3Command = "mount-s3"
+)
+
+// 采用Aws开发的一个mountpoint-s3的挂载服务，因为是采用Rust编写的，所以感觉要比golang编写的rclone要性能高
+// https://github.com/awslabs/mountpoint-s3  git地址
+
+type MountpointS3 struct {
+	bucket string
+}
+
+func NewMountpointS3(bucket string) *MountpointS3 {
+	return &MountpointS3{
+		bucket: bucket,
+	}
+}
+
+func (m *MountpointS3) Stage(path string) error {
+	klog.V(4).Info("MountpointS3 Stage method not implements")
+	return nil
+}
+
+func (m *MountpointS3) Unstage(path string) error {
+	klog.V(4).Info("MountpointS3 Unstage method not implements")
+	return nil
+}
+
+func (m *MountpointS3) Mount(source string, target string) error {
+	url := os.Getenv("ENDPOINT_URL")
+	args := []string{
+		"--endpoint-url=" + url,
+		m.bucket,
+		"--prefix=" + source,
+		target,
+	}
+
+	cmd := exec.Command(mountS3Command, args...)
+	klog.V(4).Infof("Mount fuse with command:%s and args:%s", mountS3Command, args)
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error execute fuse mount command:%s and args:%s", mountS3Command, args)
+	}
+	return nil
+}
