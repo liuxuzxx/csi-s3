080d4e9375c07b2aaa7281194edb69e3b365d43b
liuxu <liuxu@wxchina.com>
2024-03-18 10:33:01 +0800

    1.修改版本

--- cmd/s3csi/build.sh
+++ cmd/s3csi/build.sh
@@ -2,8 +2,8 @@
 
 go build
 
-docker build -f ./Dockerfile -t xwharbor.wxchina.com/cpaas-dev/component/csi-s3:release-v1.0.0.4 .
+docker build -f ./Dockerfile -t xwharbor.wxchina.com/cpaas-dev/component/csi-s3:release-v1.0.0.5 .
 
-docker push xwharbor.wxchina.com/cpaas-dev/component/csi-s3:release-v1.0.0.4
+docker push xwharbor.wxchina.com/cpaas-dev/component/csi-s3:release-v1.0.0.5
 
 rm -rf s3csi
