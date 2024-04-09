4446f68232fda0be5192d125ae01cf391ffaa890
liuxu <liuxu@wxchina.com>
2024-04-09 19:31:31 +0800

    1.计划通过参数的形式传入挂载的参数信息,但是需要确定一个格式,方便处理

---
+++ pkg/s3/rclone_test.go
@@ -0,0 +1,35 @@
+package s3
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestUseRcloneMountMinio(t *testing.T) {
+
+	envs := []string{
+		"AWS_ACCESS_KEY_ID=admin",
+		"AWS_SECRET_ACCESS_KEY=minioadmin",
+	}
+
+	args := []string{
+		"mount",
+		"liuxu:/k8s-dev-sc",
+		"/media/liuxu/data/temp/data/minio",
+		"--config=/media/liuxu/data/temp/data/rclone.conf",
+		"--s3-endpoint=http://10.20.121.41:30629",
+		"--attr-timeout=5m",
+		"--vfs-cache-mode=full --vfs-cache-max-age 24h --vfs-cache-max-size 10G --vfs-read-chunk-size-limit 100M --buffer-size 100M --file-perms 0777 --daemon",
+	}
+
+	cmd := exec.Command("rclone", args...)
+	cmd.Stderr = os.Stderr
+	cmd.Env = append(cmd.Environ(), envs...)
+	if out, err := cmd.Output(); err != nil {
+		fmt.Print(err.Error())
+	} else {
+		fmt.Printf("Rclone mount成功:%s", string(out))
+	}
+}
