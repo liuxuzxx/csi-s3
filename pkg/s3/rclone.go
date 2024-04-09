73ba1744961a7dcf20a4729ff0719defaf214bd2
liuxu <liuxu@wxchina.com>
2024-04-09 18:29:02 +0800

    1.rclone挂载的时候增加f--file-perms 0777参数配置

--- pkg/s3/rclone.go
+++ pkg/s3/rclone.go
@@ -56,6 +56,7 @@ func (r *Rclone) Mount(source string, target string) error {
 		"--vfs-cache-max-size=10G",
 		"--vfs-read-chunk-size-limit=100M",
 		"--buffer-size=100M",
+		"--file-perms 0777",
 		"--daemon",
 	}
 	envs := []string{
